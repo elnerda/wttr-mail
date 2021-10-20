21c1af29b993b555770907bad9913e536fabc9d2
Gaetano Fresa <fischkotpf@hotmail.de>
2021-10-20 14:49:11 +0200

    Update wttr_python.py
    funktion zum json parsen geschrieben.
    ausgabe der werte mit print

--- wttr_python.py
+++ wttr_python.py
@@ -1,22 +1,46 @@
-#import the necessary package!
+#packages
 import json
 import requests
-import json
 
-#fetch the weater details
+#funktionen
+
+def json_extract(obj, key):
+    """Recursively fetch values from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    values = extract(obj, arr, key)
+    return values
+
+#json holen
 url = 'https://wttr.in/Darmstadt?format=j1'
-wttr = requests.get(url)
-r=wttr.json()
-print(json.dumps(r, indent=2))
-print(r['weather'])
-for value in r['weather']:
-    print(json.dumps(value , indent=2))
-    for val in value['hourly']:
-        print(val)
-        for j in val['humidity']:
-            print(j)
+r = requests.get(url)
+wttr=r.json()
+
+#json in datei schreiben
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(wttr, f, ensure_ascii=False, indent=4)
 
 
 
 
- 
+
+
+temps=json_extract(wttr,'FeelsLikeC')
+print(temps)
+
+humidity=json_extract(wttr,'humidity')
+print('Feuchtigkeit: '+humidity[1]+'%')
